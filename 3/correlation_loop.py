a379f040e0c2412b34be1efbb31c4f7ba67d11f4
Oleksandr Zenaiev <oleksandr.zenaiev@gmail.com>
2025-10-01 12:32:03 +0200

    3 code

---
+++ 3/correlation_loop.py
@@ -0,0 +1,37 @@
+import ROOT
+import array
+
+nrepeat = 1000
+f = ROOT.TFile('ttbarSel_merged.root')
+t = f.Get('tree')
+t.SetBranchStatus("*", 0)
+t.SetBranchStatus("mcT", 1)
+t.SetBranchStatus("mcTbar", 1)
+mcT = array.array('f', [0.]*4)
+t.SetBranchAddress('mcT', mcT)
+mcTbar = array.array('f', [0.]*4)
+t.SetBranchAddress('mcTbar', mcTbar)
+n = t.GetEntries()
+mean_x = 0
+mean_y = 0
+mean_xx = 0
+mean_yy = 0
+mean_xy = 0
+for irepeat in range(nrepeat):
+  for i in range(n):
+    t.GetEntry(i)
+    #print(mcT, mcTbar)
+    mean_x += mcT[0]
+    mean_y += mcTbar[0]
+    mean_xx += mcT[0]**2
+    mean_yy += mcTbar[0]**2
+    mean_xy += mcT[0]*mcTbar[0]
+mean_x /= n
+mean_y /= n
+mean_xx /= n
+mean_yy /= n
+mean_xy /= n
+sigma_x = (mean_xx - mean_x**2)**0.5
+sigma_y = (mean_yy - mean_y**2)**0.5
+cor = (mean_xy - mean_x) / (sigma_x * sigma_y)
+print(f'cor = {cor}')
---
+++ 3/correlation_rdf.py
@@ -0,0 +1,21 @@
+import ROOT
+ROOT.EnableImplicitMT()
+
+#rdf = ROOT.RDataFrame('tree', 'ttbarSel_merged.root')
+rdf = ROOT.RDataFrame('tree', ['ttbarSel_merged.root']*1000)
+rdf = rdf.Define('x', 'mcT[0]')
+rdf = rdf.Define('y', 'mcTbar[0]')
+rdf = rdf.Define('xx', 'x*x')
+rdf = rdf.Define('yy', 'y*y')
+rdf = rdf.Define('xy', 'x*y')
+mean_x = rdf.Mean('x')
+mean_y = rdf.Mean('y')
+mean_xx = rdf.Mean('xx')
+mean_yy = rdf.Mean('yy')
+mean_xy = rdf.Mean('xy')
+sigma_x = (mean_xx.GetValue() - mean_x.GetValue()**2)**0.5
+sigma_y = (mean_yy.GetValue() - mean_y.GetValue()**2)**0.5
+#cor = (mean_xy - mean_x) / (sigma_x * sigma_y)
+cor = (mean_xy.GetValue() - mean_x.GetValue()) / (sigma_x * sigma_y)
+print(f'cor = {cor}')
+print(f'number of runs: rdf = {rdf.GetNRuns()}')
